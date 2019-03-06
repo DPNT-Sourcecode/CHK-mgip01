bce72dc8104806f94586b6fe0e352cf70a6d88a1
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 20:02:53 +0000

    Wed Mar 06 20:02:53 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,16 +2,26 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        // skus control
+        // skus control, null is a illegal input
         if (skus == null){
             return -1;
         } else if(skus.length()<=0){
             return 0;
         }
+
+        // have items, creation of list
+        List<String> products = Stream.of(skus.split("")).collect(Collectors.toList());
+
+
         return 0;
         //throw new SolutionNotImplementedException();
     }
 }
+
 
