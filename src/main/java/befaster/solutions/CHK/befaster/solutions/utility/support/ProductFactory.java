998606f959f147f152d19fce98bff9779bdb0a22
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 20:47:32 +0000

    Wed Mar 06 20:47:32 UTC 2019

--- src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/ProductFactory.java
+++ src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/ProductFactory.java
@@ -1,4 +1,9 @@
 package befaster.solutions.CHK.befaster.solutions.utility.support;
 
-public class ProductFactory {
+public class ProductFactory implements IFactory{
+    @Override
+    public Product createProduct(product p) {
+        return null;
+    }
 }
+
