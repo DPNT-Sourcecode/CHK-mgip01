190806cc6d49622db5782771c17721126f45a6a3
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 21:59:31 +0000

    Wed Mar 06 21:59:31 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,10 +4,217 @@ import befaster.runner.SolutionNotImplementedException;
 import befaster.solutions.CHK.befaster.solutions.utility.support.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+/*
+ * Enums constants to represent the items and the attributes of these items such as offers and special offers
+ */
+
+// item representation
+enum item {
+    // itemRef, price, haveOffer, haveSpecialOffer, haveCombOffer
+    A("A", 50, true, false, false), // 3A for 130, 5A for 200
+    B("B", 30, true, false, false), // 2B for 45
+    C("C", 20, false, false, false), D("D", 15, false, false, false), E("E", 40, false, true, false), // 2E get one B
+    // free
+    F("F", 10, false, true, false), // 2F get one F free
+    G("G", 20, false, false, false), H("H", 10, true, false, false), // 5H for 45, 10H for 80 |
+    I("I", 35, false, false, false), J("J", 60, false, false, false), K("K", 70, true, false, false), // | 2K for 150 |
+    L("L", 90, false, false, false), M("M", 15, false, false, false), N("N", 40, false, true, false), // | 3N get one M
+    // free |
+    O("O", 10, false, false, false), P("P", 50, true, false, false), // | 5P for 200 |
+    Q("Q", 30, true, false, false), // | 3Q for 80 |
+    R("R", 50, false, true, false), // | 3R get one Q free |
+    S("S", 20, false, false, true), // buy any 3 of (S,T,X,Y,Z) for 45, 3S = 45
+    T("T", 20, false, false, true), // buy any 3 of (S,T,X,Y,Z) for 45, 3T = 45
+    U("U", 40, false, true, false), // | 3U get one U free |
+    V("V", 50, true, false, false), // | 2V for 90, 3V for 130 |
+    W("W", 20, false, false, false), X("X", 17, false, false, true), // buy any 3 of (S,T,X,Y,Z) for 45, 3X = 45
+    Y("Y", 20, false, false, true), // buy any 3 of (S,T,X,Y,Z) for 45, 3Y = 45
+    Z("Z", 21, false, false, true); // buy any 3 of (S,T,X,Y,Z) for 45, 3Z = 45
+    private String itemRef;
+    private int price;
+    private boolean haveOffer;
+    private boolean haveSpecialOffer;
+    private boolean haveCombOffer;
+
+    // contructor
+    item(final String itemRef, final int price, final boolean haveOffer, final boolean haveSpecialOffer,
+         final boolean haveCombOffer) {
+        this.itemRef = itemRef;
+        this.price = price;
+        this.haveOffer = haveOffer;
+        this.haveSpecialOffer = haveSpecialOffer;
+        this.haveCombOffer = haveCombOffer;
+    }
+
+    public String getItemRef() {
+        return itemRef;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isHaveOffer() {
+        return haveOffer;
+    }
+
+    public boolean isHaveSpecialOffer() {
+        return haveSpecialOffer;
+    }
+
+    public boolean isHaveCombOffer() {
+        return haveCombOffer;
+    }
+
+    // get offers by item
+    public ArrayList<offer> getOffers() {
+        ArrayList<offer> offers = new ArrayList<offer>();
+        if (this.equals(A)) {
+            offers.add(offer.A1);
+            offers.add(offer.A2);
+        } else if (this.equals(B)) {
+            offers.add(offer.B);
+        } else if (this.equals(H)) {
+            offers.add(offer.H1);
+            offers.add(offer.H2);
+        } else if (this.equals(K)) {
+            offers.add(offer.K);
+        } else if (this.equals(P)) {
+            offers.add(offer.P);
+        } else if (this.equals(Q)) {
+            offers.add(offer.Q);
+        } else if (this.equals(V)) {
+            offers.add(offer.V1);
+            offers.add(offer.V2);
+        } else if (this.equals(S)) {
+            offers.add(offer.S);
+        } else if (this.equals(T)) {
+            offers.add(offer.T);
+        } else if (this.equals(X)) {
+            offers.add(offer.X);
+        } else if (this.equals(Y)) {
+            offers.add(offer.Y);
+        } else if (this.equals(Z)) {
+            offers.add(offer.Z);
+        }
+
+        return offers;
+    }
+
+    // get specialOffers by item
+    public ArrayList<specialOffer> getSpecialOffers() {
+        ArrayList<specialOffer> specialOffers = new ArrayList<specialOffer>();
+        if (this.equals(E)) {
+            specialOffers.add(specialOffer.E);
+        } else if (this.equals(F)) {
+            specialOffers.add(specialOffer.F);
+        } else if (this.equals(N)) {
+            specialOffers.add(specialOffer.N);
+        } else if (this.equals(R)) {
+            specialOffers.add(specialOffer.R);
+        } else if (this.equals(U)) {
+            specialOffers.add(specialOffer.U);
+        }
+
+        return specialOffers;
+    }
+
+    // get combination offers
+    public ArrayList<combinationOffer> getCombinationOffers() {
+        ArrayList<combinationOffer> combOffers = new ArrayList<combinationOffer>();
+        if (this.equals(S)) {
+            combOffers.add(combinationOffer.S);
+        } else if (this.equals(T)) {
+            combOffers.add(combinationOffer.T);
+        } else if (this.equals(X)) {
+            combOffers.add(combinationOffer.X);
+        } else if (this.equals(Y)) {
+            combOffers.add(combinationOffer.Y);
+        } else if (this.equals(Z)) {
+            combOffers.add(combinationOffer.Z);
+        }
+        return combOffers;
+    }
+}
+
+// offer linked to item
+enum offer {
+    // to manage the groups of items of the same item
+    // will be necessary manage the groups in descendent order by numItems
+    // numItems, price
+    A1(5, 200), A2(3, 130), B(2, 45), H1(10, 80), H2(5, 45), K(2, 120), P(5, 200), Q(3, 80), V1(3, 130), V2(2, 90),
+    S(3, 45), T(3, 45), X(3, 45), Y(3, 45), Z(3, 45);
+    private int numItems;
+    private int price;
+
+    // constructor
+    offer(final int numItems, final int price) {
+        this.numItems = numItems;
+        this.price = price;
+    }
+
+    public int getNumItems() {
+        return numItems;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
+// specialOffer linked to item
+enum specialOffer {
+    // numItems, free item object
+    E(2, item.B), F(2, item.F), N(3, item.M), R(3, item.Q), U(3, item.U);
+
+    private int numItems;
+    private item freeItem;
+
+    // constructor
+    specialOffer(final int numItems, final item freeItem) {
+        this.numItems = numItems;
+        this.freeItem = freeItem;
+    }
+
+    public int getNumItems() {
+        return numItems;
+    }
+
+    public item getFreeItem() {
+        return freeItem;
+    }
+}
+
+enum combinationOffer {
+    S(3,45, Arrays.asList(item.T, item.X, item.Y, item.Z)), T(3,45, Arrays.asList(item.S, item.X, item.Y, item.Z)),
+    X(3,45, Arrays.asList(item.T, item.S, item.Y, item.Z)), Y(3,45, Arrays.asList(item.T, item.X, item.S, item.Z)),
+    Z(3,45, Arrays.asList(item.T, item.X, item.Y, item.S));
+    private int numItems;
+    private int price;
+    private List<item> itemsRefList;
+
+    combinationOffer(final int numItems, final int price, final List<item> itemsRefList) {
+        this.numItems = numItems;
+        this.price = price;
+        this.itemsRefList = itemsRefList;
+    }
+
+    public int getNumItems() {
+        return numItems;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public List<item> getItemsRefList() {
+        return itemsRefList;
+    }
+}
 
 public class CheckoutSolution {
 
@@ -84,6 +291,7 @@ public class CheckoutSolution {
         return getTotalPrice();
     }
 }
+
 
 
 
