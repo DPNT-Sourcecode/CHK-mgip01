d37a2079312346d97d43f1dd7f6a9940596ba6a9
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 20:29:32 +0000

    Wed Mar 06 20:29:32 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/CheckoutSolutionR1.java
@@ -0,0 +1,51 @@
+package befaster.solutions.CHK;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CheckoutSolutionR1 {
+    public Integer checkout(String skus) {
+        // skus control, null is a illegal input
+        if (skus == null){
+            return -1;
+        } else if(skus.length()<=0){
+            return 0;
+        }
+        // legal products
+        List<String> references = new ArrayList<String>();
+        references.add("A");
+        references.add("B");
+        references.add("C");
+        references.add("D");
+
+        // have items, creation of list
+        List<String> products = Stream.of(skus.split("")).collect(Collectors.toList());
+
+        // validation
+        List<String> cloneProducts = products.stream().collect(Collectors.toList());
+
+        // remove all legal products, if the size of list is greater than 0, exist illegal reference of products
+        for(String ref : references) {
+            cloneProducts.removeIf(p -> p.equals(ref));
+        }
+
+        if(cloneProducts.size()>0){
+            return -1;
+        }
+
+        // calculation of prices
+        Integer priceProductA = ((Collections.frequency(products, "A") / 3)*130) + ((Collections.frequency(products, "A") % 3)*50);
+
+        Integer priceProductB = ((Collections.frequency(products, "B") / 2)*45) + ((Collections.frequency(products, "B") % 2)*30);
+
+        Integer priceProductC = Collections.frequency(products, "C")*20;
+
+        Integer priceProductD = Collections.frequency(products, "D")*15;
+
+        return priceProductA + priceProductB + priceProductC + priceProductD;
+        //throw new SolutionNotImplementedException();
+    }
+}
---
+++ src/test/java/jsanchez/solution/checkout/test/CheckoutSolutionR2Test.java
@@ -0,0 +1,36 @@
+package jsanchez.solution.checkout.test;
+
+import befaster.solutions.CHK.CheckoutSolution;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutSolutionR2Test {
+
+    private CheckoutSolution checkout;
+
+    @Before
+    public void setUp(){
+        checkout = new CheckoutSolution();
+    }
+
+    @Test
+    public void simpleTest(){
+        // item A
+        assertThat(checkout.checkout("AAA"), equalTo(130));
+        assertThat(checkout.checkout("AAAAAA"), equalTo(260));
+        assertThat(checkout.checkout("AAAAA"), equalTo(230));
+        // item B
+        assertThat(checkout.checkout("BB"), equalTo(45));
+        assertThat(checkout.checkout("BBBB"), equalTo(90));
+        assertThat(checkout.checkout("BBB"), equalTo(75));
+        // item C
+        assertThat(checkout.checkout("CC"), equalTo(40));
+        assertThat(checkout.checkout("CCC"), equalTo(60));
+        // item D
+        assertThat(checkout.checkout("DD"), equalTo(30));
+        assertThat(checkout.checkout("DDDD"),equalTo(60));
+    }
+}
