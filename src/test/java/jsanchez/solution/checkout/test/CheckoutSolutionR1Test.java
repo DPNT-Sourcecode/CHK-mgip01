7873af54c3708518c948497418f1ff26c0b961c5
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 19:56:53 +0000

    Wed Mar 06 19:56:53 UTC 2019

--- src/test/java/jsanchez/solution/checkout/test/CheckoutSolutionR1Test.java
+++ src/test/java/jsanchez/solution/checkout/test/CheckoutSolutionR1Test.java
@@ -1,4 +1,27 @@
 package jsanchez.solution.checkout.test;
 
+import befaster.solutions.CHK.CheckoutSolution;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionR1Test {
+
+    private CheckoutSolution checkout;
+
+    @Before
+    public void setUp(){
+        checkout = new CheckoutSolution();
+    }
+
+    @Test
+    public void simpleTest(){
+        assertThat(checkout.checkout("AAA"), equalTo(130));
+        assertThat(checkout.checkout("AAAAAA"), equalTo(230));
+        assertThat(checkout.checkout("AAAAA"), equalTo(230));
+
+    }
 }
+
