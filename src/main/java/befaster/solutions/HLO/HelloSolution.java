fc72cf60eaa31d24c76d54347098b9485dd91ed7
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 19:48:39 +0000

    Wed Mar 06 19:48:39 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -4,9 +4,10 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        //StringBuffer sb = new StringBuffer("Hello, ").append(friendName).append("!");
-        StringBuffer sb = new StringBuffer("Hello, World!");
+        StringBuffer sb = new StringBuffer("Hello, ").append(friendName).append("!");
+        //StringBuffer sb = new StringBuffer("Hello, World!");
         return sb.toString();
         //throw new SolutionNotImplementedException();
     }
 }
+
