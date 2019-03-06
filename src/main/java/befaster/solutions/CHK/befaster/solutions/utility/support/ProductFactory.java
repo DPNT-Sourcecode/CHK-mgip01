6e0c51348d9e103333286c4195b8b703788afc25
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 20:53:32 +0000

    Wed Mar 06 20:53:32 UTC 2019

--- src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/ProductFactory.java
+++ src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/ProductFactory.java
@@ -2,13 +2,24 @@ package befaster.solutions.CHK.befaster.solutions.utility.support;
 
 public class ProductFactory implements IFactory{
     @Override
-    public Product createProduct(product p) {
+    public Product createProduct(product productRef) {
 
-        switch (p){
-            return new Product()
+        switch (productRef){
+            case A:
+                return new Product(productRef, 50);
+            case B:
+                return new Product(productRef, 30);
+            case C:
+                return new Product(productRef, 20);
+            case D:
+                return new Product(productRef, 15);
+            case E:
+                return new Product(productRef, 40);
+                default:
+                    return null;
         }
-        return null;
     }
 }
+
 
 
