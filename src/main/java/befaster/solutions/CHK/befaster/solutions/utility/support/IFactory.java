946fa4af5a7a2b7273a90704938d59720d026566
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 20:41:31 +0000

    Wed Mar 06 20:41:31 UTC 2019

--- src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/IFactory.java
+++ src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/IFactory.java
@@ -1,5 +1,24 @@
 package befaster.solutions.CHK.befaster.solutions.utility.support;
 
 public interface IFactory {
+    public enum product {
+        A("A"),
+        B("B"),
+        C("C"),
+        D("D"),
+        C("C"),
+        E("E");
+
+        private String ref;
+        product(final String ref){
+            this.ref = ref;
+        }
+
+        public String getRef(){
+            return ref;
+        }
+    }
+
     
 }
+
