60535f644e126d9cc456af7c111f55c09e17e340
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 21:05:32 +0000

    Wed Mar 06 21:05:32 UTC 2019

--- src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/OfferFactory.java
+++ src/main/java/befaster/solutions/CHK/befaster/solutions/utility/support/OfferFactory.java
@@ -1,5 +1,19 @@
 package befaster.solutions.CHK.befaster.solutions.utility.support;
 
+import java.util.Arrays;
+
 public class OfferFactory implements IOfferFactory {
 
+    @Override
+    public Offer createOffer(EOffer eOffer) {
+        switch (eOffer){
+            case A:
+                return new Offer(3,130,null);
+            case B:
+                return new Offer(2,45, null);
+            case E:
+                return new Offer(2, 0, Arrays.asList())
+        }
+    }
 }
+
