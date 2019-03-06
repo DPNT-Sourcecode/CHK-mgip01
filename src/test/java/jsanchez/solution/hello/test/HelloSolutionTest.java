9989ebadccf8c793884c5d9f0124487435cb76f6
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-06 19:43:21 +0000

    Wed Mar 06 19:43:21 UTC 2019

---
+++ src/test/java/jsanchez/solution/hello/test/HelloSolutionTest.java
@@ -0,0 +1,20 @@
+package jsanchez.solution.hello.test;
+
+import befaster.solutions.HLO.HelloSolution;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloSolutionTest {
+
+    private HelloSolution hello;
+
+    @Before
+    public void setUp(){
+        hello = new HelloSolution();
+    }
+
+    @Test
+    public void helloTest(){
+        assert
+    }
+}
