cac6c22c9c9b60f81f8adef8359d76c03142d809
sbx_user1059 <sbx_user1059@ip-10-128-58-173.eu-west-2.compute.internal>
2019-03-07 00:42:21 +0000

    Thu Mar 07 00:42:21 UTC 2019

--- src/test/java/jsanchez/solution/checkout/test/CheckoutSolutionR5Test.java
+++ src/test/java/jsanchez/solution/checkout/test/CheckoutSolutionR5Test.java
@@ -32,9 +32,9 @@ public class CheckoutSolutionR5Test {
         //assertThat(checkout.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),equalTo(853));
         //assertThat(checkout.checkout("X"), equalTo(17));
         //assertThat(checkout.checkout("YZTSYX"), equalTo(90));
-        assertThat(checkout.checkout("KK"), equalTo(120));
-        assertThat(checkout.checkout("KKKK"), equalTo(240));
-        assertThat(checkout.checkout("KKK"), equalTo(190));
+        assertThat(checkout.checkout("SSSZ"), equalTo(65));
+        assertThat(checkout.checkout("ZZZS"), equalTo(65));
+        assertThat(checkout.checkout("STXS"), equalTo(62));
     }
 
     @Test
@@ -109,4 +109,5 @@ public class CheckoutSolutionR5Test {
         assertThat(checkout.checkout("BBEE"), equalTo(110));
     }
 }
+
 
