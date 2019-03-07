e98b0efcbdfffe629606f5c49b388d67e3ca00a3
sbx_user1059 <sbx_user1059@ip-10-129-203-59.eu-west-2.compute.internal>
2019-03-07 20:07:38 +0000

    Thu Mar 07 20:07:38 UTC 2019

--- src/test/java/jsanchez/solution/checkout/test/CheckoutSolutionR5Test.java
+++ src/test/java/jsanchez/solution/checkout/test/CheckoutSolutionR5Test.java
@@ -30,10 +30,10 @@ public class CheckoutSolutionR5Test {
     public void isolateTest(){
         // impossible with the combination offer, because the patter is met!
         //assertThat(checkout.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),equalTo(853));
-        //assertThat(checkout.checkout("X"), equalTo(17));
-        assertThat(checkout.checkout("YZTSYX"), equalTo(90));
-        //assertThat(checkout.checkout("SSSZ"), equalTo(65));
-        //assertThat(checkout.checkout("ZZZS"), equalTo(65));
+        assertThat(checkout.checkout("X"), equalTo(17));
+        //assertThat(checkout.checkout("YZTSYX"), equalTo(90));
+        assertThat(checkout.checkout("SSSZ"), equalTo(65));
+        assertThat(checkout.checkout("ZZZS"), equalTo(65));
         //
         //assertThat(checkout.checkout("STXS"), equalTo(62));
     }
@@ -110,4 +110,5 @@ public class CheckoutSolutionR5Test {
         assertThat(checkout.checkout("BBEE"), equalTo(110));
     }
 }
+
 
